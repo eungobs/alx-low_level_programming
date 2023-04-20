b1ed9240b46f1e614ea8a33977dc7903bddb4feb
eungobs <eungobs@gmail.com>
2023-04-20 05:50:59 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Desciption: main - prints to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+puts("\"programming is like building a multingual puzzle");
+return (0);
+}
