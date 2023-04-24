88380664eca0533e3e91b8762bd37784a912aef8
eungobs <eungobs@gmail.com>
2023-04-24 12:01:22 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main -A c program that prints a line to the standard error
+ * Return 1 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n" 59);
-size of("and that piece of art is useful\" -Dora Korpar, 2015-10-19"));
-return (1);
+	fputs("and that place of art is useful\" -Dpra Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
-
