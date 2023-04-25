0884b4656fe5ef0e598dbacd1c1f6ae1e4657962
eungobs <eungobs@gmail.com>
2023-04-25 05:25:22 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - Entry point of my code
+ *
+ * Return Always 0 (Success)
+ */
+
+int main(void)
+{
+-putchar('_');
+
+-putchar('p');
+
+-putchar('u');
+
+-putchar('t');
+
+-putchar('c');
+
+-putchar('h');
+
+-putchar('a');
+
+-putchar('r');
+
+-putchar('\n');
+
+return (0);
+}
