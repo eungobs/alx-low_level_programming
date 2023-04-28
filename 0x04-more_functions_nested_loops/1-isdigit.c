af8762bf80a8e0cb1e2cfa2b9a5fa77e0d3bd99c
eungobs <eungobs@gmail.com>
2023-04-28 07:51:49 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,16 +6,11 @@
  *  Return: 1 for a chacked that will be a digit or 0 for any else
  */
 
-int _isdigit(int x);
+int _isdigit(int x)
 {
 if (x >= 48 && x <= 57)
 
-{
-
 return (1);
 
-}
-
 return (0);
-
 }
