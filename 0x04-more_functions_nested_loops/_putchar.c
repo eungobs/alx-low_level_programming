5b7b6fec5a2fcdeed07ad08fb3358d043ef2b4cd
eungobs <eungobs@gmail.com>
2023-04-28 09:48:39 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,13 +1,15 @@
 #include "main.h"
 #include <unistd.h>
-/**
- * _putchar -writes the character c to stdout
+/**i
+ * _putchar - writes the character c to stdout
  * @c The character to print
  *
  * Return: on success 1
  * On error, -1 is returned, and error is set appropriately.
  */
-int_putchar.c(char c)
+
+int_putchar c(char c)
 {
+
 retur (write(1, &c, 1));
 }
