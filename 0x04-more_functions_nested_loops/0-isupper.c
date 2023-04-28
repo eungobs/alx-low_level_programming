a4878cc61712a6f8531c9646d901ff45a51267ed
eungobs <eungobs@gmail.com>
2023-04-28 05:31:16 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ *  _isupper - uppercase letters
+ *  @c: char to check
+ *
+ *  Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		retur (0);
+}
