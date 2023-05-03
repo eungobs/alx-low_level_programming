83128e7a9af7a65982d3097267b46fbd794743ac
eungobs <eungobs@gmail.com>
2023-05-03 12:15:30 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,15 +2,15 @@
 
 /**
  * _puts - prints a string
- *@str: The string to print
+ * @str: The string to print
  * Return: void
  */
+
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
-        	
-        _putchar;
-        
+for (; *str != '\0'; str++)
+{
+_putchar(*str);
+	}
 	_putchar('\n');
-	
 }
