563a45c47749519e4c17bfab62dbaf93c079f6af
eungobs <eungobs@gmail.com>
2023-05-10 10:34:34 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
 * _print_rev_recursion - Prints a string in reverse
@@ -12,6 +12,6 @@ void _print_rev_recursion(char *s)
 if (*s != '\0')
 {
 _print_rev_recursion(s + 1);
-_putchar(*s);
 }
+ putchar(*s);
 }
