7a6a87525c838fb259b9d9199e88a1dcfb0a05fb
eungobs <eungobs@gmail.com>
2023-05-10 10:39:33 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,11 @@
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
-void _print_rev_recursion(char *s)
+void_print_rev_recursion(char *s)
 {
 if (*s != '\0')
 {
 _print_rev_recursion(s + 1);
 }
- putchar(*s);
+putchar(*s);
 }
