ac0c37cf6e4bad10d795155970ac3e4317c27dc7
eungobs <eungobs@gmail.com>
2023-05-12 09:33:58 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,29 +1,27 @@
 #include "main.h"
 
 /**
- * print_squre - prints n squres according n number of times
- * @size: The number of squres/number of times
- * Return: empty
- */
-
+* print_square -  checks for checks for a digit (0 through 9).
+* @size: size -  Variable
+* Return: Always 0.
+*/
 void print_square(int size)
-
 {
-	int x, y;
+int x, y;
 
-	if (size <= 0)
-	{
-	_putchar('\n');
-	}
-	else
-	{
-	for (x = 0; x < size; x++)
-	{
-	for (y = 0; y < size; y++)
-	{
-	_putchar(35);
-	}
-	_putchar('\n');
-	}
-	}
+if (size > 0)
+{
+for (x = 1; x <= size; x++)
+{
+for (y = 1; y <= size; y++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
