50ec0a3c40cfb04b77a138e62092cdfc31559177
eungobs <eungobs@gmail.com>
2023-05-12 09:42:04 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,27 +1,34 @@
 #include "main.h"
+
 /**
- * print_squre - prints n squres according n number of times
- * @size: The number of squres/number of times
- * Return: empty
- */
-
+* print_triangle -  checks for checks for a digit (0 through 9).
+* @size: size -  Variable
+* Return: Always 0.
+*/
 void print_triangle(int size)
-
 {
-
 int x, y;
 
-if (size <= 0)
-
-_putchar('\n');
-
+if (size > 0)
+{
+for (x = 1; x <= size; x++)
+{
+for (y = 1; y <= size; y++)
+{
+if (y <= size - x)
+{
+_putchar(' ');
+}
 else
-
-for (x = 0; x < size; x++)
-
-for (y = 0; y < size; y++)
-
-_putchar(35);
-
+{
+_putchar('#');
+}
+}
 _putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
