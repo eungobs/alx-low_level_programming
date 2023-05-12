75167cc2526aa1f21b8d1f15ec43751e39764d33
eungobs <eungobs@gmail.com>
2023-05-12 09:26:03 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,24 +1,20 @@
 #include "main.h"
 
 /**
- * print_line - Draws a straight line according to parameter
- * @n: The number of lines to draw
- * Return: empty
- */
-
+* print_line -  checks for checks for a digit (0 through 9).
+* @n: n -  Variable
+* Return: Always 0.
+*/
 void print_line(int n)
-
 {
-	int x;
+int x;
 
-	if (n <= 0)
-	{
-	_putchar('\n');
-	}
-	else
-	{
-	for (x = 0; x < n; x++)
-	_putchar(95);
-	}
+if (n > 0)
+{
+for (x = 1; x <= n; x++)
+{
+_putchar(95);
+}
+}
 _putchar('\n');
 }
