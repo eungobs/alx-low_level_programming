f1c58a3dafe8d584f6aff70f05b8bec201df7211
eungobs <eungobs@gmail.com>
2023-05-22 12:20:44 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - prints the name of the file it was compiled from
- * Return: everything
+ * Return: everything worked ok
  * */
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n",  __BASE_FILE__);
 	return (0);
