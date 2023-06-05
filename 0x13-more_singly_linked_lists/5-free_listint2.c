c65387af90ccba40221ee3ec3b9d3091b69889b0
eungobs <engobd@gmail.com>
2023-06-05 13:24:58 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,19 +5,19 @@
 *@head: pointer to the head of the list
 *Return: void
 */
-
 void free_listint2(listint_t **head)
 {
-listint_t *cursor;
-listint_t **temp = head;
-if (temp != NULL)
-{
-while (*head != NULL)
-{
-cursor = *head;
-free(cursor);
-*head = (*head)->next;
-}
-*temp = NULL;
-}
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
 }
