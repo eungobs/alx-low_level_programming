0b8277e51d54e23eca59d34eb915c0e2d2d71d50
eungobs <engobd@gmail.com>
2023-06-05 13:16:50 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,9 +7,13 @@
 */
 void free_listint(listint_t *head)
 {
-listint_t *temp;
-while (head != NULL)
-temp = head;
-head = head->next;
-free(temp);
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
+
