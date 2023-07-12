b2a01c8cf2b4f5d78aeaae9b7f38f64c08df9338
eungobs <eungobs@gmail.com>
2023-07-12 15:41:59 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -288,7 +288,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
-	r = read(op, header, sizeof(Elf64_Ehdr));
+	re = read(op, header, sizeof(Elf64_Ehdr));
 	if (re == -1)
 	{
 		free(header);
