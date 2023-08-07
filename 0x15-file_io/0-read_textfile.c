f4d8a2da373f41043afad3e81705ec26b50e88cc
eungobs <eungobs@gmail.com>
2023-08-07 09:59:02 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,31 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+
 /**
-* read_textfile - that reads a text file and prints
-* @filename: variable pointer
-* @letters: size letters
-* Description: Write a function that reads a text file and prints it
-* to the POSIX standard output.
-* Return: the actual number of letters it could read and print, 0 otherwise
+* read_textfile - read a textfile.
+* @filename: name of the file to open.
+* @letters: number of letters to print in the standar ouput.
+*
+* Return: number of letter printed.
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t file;
-char *text;
-
-text = malloc(letters);
-if (text == NULL)
-return (0);
+int fd_read, count_chars, fd_open;
+char *buf_letters;
 
 if (filename == NULL)
 return (0);
-
-file = open(filename, O_RDONLY);
-
-if (file == -1)
-
-free(text);
+fd_open = open(filename, O_RDONLY);
+if (fd_open == -1)
 return (0);
+buf_letters = malloc(sizeof(char) * letters);
+if (buf_letters == NULL)
+return (0);
+fd_read = read(fd_open, buf_letters, letters);
+if (fd_read == -1)
+{
+free(buf_letters);
+return (0);
+}
+count_chars = write(STDOUT_FILENO, buf_letters, fd_read);
+if (count_chars == -1)
+{
+free(buf_letters);
+return (0);
+}
+close(fd_open);
+free(buf_letters);
+return (count_chars);
 }
