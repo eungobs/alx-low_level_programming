9f6c0ed8e6c98be996a03b8d15f485383dcc4f26
eungobs <eungobs@gmail.com>
2023-08-17 09:27:22 -0700

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,23 +2,23 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Returns the number of elements in a doubly linked list.
- * @h: Pointer to the head node of the list.
- *
- * Description: function to return number of elements in a linked list.
- * Return: Number of elements in the list.
- */
+* dlistint_len - Returns the number of elements in a doubly linked list.
+* @h: Pointer to the head node of the list.
+*
+* Description: function to return number of elements in a linked list.
+* Return: Number of elements in the list.
+*/
 
-size_t dlistint_len(const dlistint_t *h) 
+size_t dlistint_len(const dlistint_t *h)
 {
-    const dlistint_t *current = h;
-    size_t element_count = 0;
+const dlistint_t *current = h;
+size_t element_count = 0;
 
-    while (current != NULL) 
-    {
-        current = current->next;
-        element_count++;
-    }
+while (current != NULL)
+{
+current = current->next;
+element_count++;
+}
 
-    return element_count;
+return (element_count);
 }
