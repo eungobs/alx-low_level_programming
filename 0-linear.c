a3e5ad05549540355bbb8c525a008cb950634292
eungobs <eungobs@gmail.com>
2023-12-13 12:14:29 -0800

    0-linear.c

--- 0-linear.c
+++ 0-linear.c
@@ -2,27 +2,29 @@
 #include <stdio.h>
 
 /**
- * linear_search - Searches for a value in an array of integers using Linear Search algorithm
- *
- * @array: Array to search
- * @size: Size of the array
- * @value: Value to search for
- *
- * Return: First index where value is located or -1 if value is not present or if array is NULL
- */
+* linear_search - Searches for a value in an array of integers using
+*                 Linear Search algorithm
+*
+* @array: Array to search
+* @size: Size of the array
+* @value: Value to search for
+*
+* Return: First index where value is located or -1 if value is not
+*                                    present or if array is NULL
+*/
 int linear_search(int *array, size_t size, int value)
 {
-    unsigned int index;
+unsigned int index;
 
-    for (index = 0; array && index < size; index++)
-    {
-        printf("Value checked array[%d] = [%d]\n", index, array[index]);
+for (index = 0; array && index < size; index++)
+{
+printf("Value checked array[%d] = [%d]\n", index, array[index]);
 
-        if (array[index] == value)
-        {
-            return (index);
-        }
-    }
+if (array[index] == value)
+{
+return (index);
+}
+}
 
-    return (-1);
+return (-1);
 }
